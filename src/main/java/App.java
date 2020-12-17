afeb9f16ed523e7205a240bec8bd2fb45d024051
ShemBrooklyne <shemnyatti@gmail.com>
2020-12-18 02:30:15 +0300

    Post, Get and Delete methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,17 @@
 import com.google.gson.Gson;
 import dao.Sql2oArticlesDao;
 import dao.Sql2oTutorialsDao;
+import exceptions.ApiExceptions;
+import model.Articles;
+import model.Tutorials;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
-import static spark.Spark.get;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.after;
 
 public class App {
 
@@ -22,7 +29,55 @@ public class App {
         TutorialsDao = new Sql2oTutorialsDao(sql2o);
         conn = sql2o.open();
 
+        // Welcome screen Json format
         get("/", "application/json", (req, res) ->
-                "{\"message\":\"Hello great Farmer! Welcome On-Board in the pursuits of solving world hunger. \n ZERO WORLD HUNGER!!!\"}");
+                "{\"Perpertual Motion Squad message\":\"Hello great Farmer! Welcome On-Board in the pursuits of solving world hunger. \n ZERO WORLD HUNGER!!!\"}");
+
+        // POST Requests to the sqlDB
+
+        // Add data into Articles table
+        post("/articles/new", "application/json", ((request, response) -> {
+            Articles articles = gson.fromJson(request.body(), Articles.class);
+            ArticlesDao.add(articles);
+            response.status(201);
+            return gson.toJson(articles);
+        }));
+
+        // Add data into Tutorials table
+        post("tutorials/new", "application/json", ((request, response) -> {
+            Tutorials tutorials = gson.fromJson(request.body(), Tutorials.class);
+            TutorialsDao.add(tutorials);
+            response.status(201);
+            return gson.toJson(tutorials);
+        }));
+
+        // GET Requests from the sqlDB
+
+        // Requests data from Articles table
+        get("/articles", "application/json", ((request, response) -> {
+            System.out.println(ArticlesDao.getAll());
+            return gson.toJson(ArticlesDao.getAll());
+        }));
+
+        // Request data from Tutorials table
+        get("/tutorials", "application/json", ((request, response) -> {
+            System.out.println(TutorialsDao.getAll());
+            return gson.toJson(TutorialsDao.getAll());
+        }));
+
+        //FILTERS
+        exception(ApiExceptions.class, (exc, req, res) -> {
+            ApiExceptions err = exc;
+            Map<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("status", err.getStatusCode());
+            jsonMap.put("errorMessage", err.getMessage());
+            res.type("application/json"); //after does not run in case of an exception.
+            res.status(err.getStatusCode()); //set the status
+            res.body(gson.toJson(jsonMap));  //set the output.
+        });
+
+        after((req, res) ->{
+            res.type("application/json");
+        });
     }
 }
